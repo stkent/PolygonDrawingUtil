c3d70a725839117a44bc9c592288022d12efa7e5
Stuart Kent <skent.dev@gmail.com>
2018-05-11 16:53:25 -0400

    Update copyright year

--- app/src/main/java/com/stkent/polygondrawingutildemo/MainActivity.java
+++ app/src/main/java/com/stkent/polygondrawingutildemo/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Stuart Kent
+ * Copyright 2018 Stuart Kent
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.
